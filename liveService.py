16862b107406d9ab36e51fe2153ff2f9c5d69c60
lxu <lawrencexu8@gmail.com>
2025-09-06 13:36:54 +0200

    adding ci/cd

--- liveService.py
+++ liveService.py
@@ -8,16 +8,19 @@ app = Flask(__name__)
 REQUEST_TIME = Summary('request_seconds', 'Time spent processing request')
 REQUEST_COUNT = Counter('request_total', 'Total HTTP GET requests')
 
+
 @app.route('/')
 @REQUEST_TIME.time()
 def hello():
     REQUEST_COUNT.inc()
     return 'Hello, World!'
 
+
 @app.route('/health')
 def health():
     REQUEST_COUNT.inc()
     return "OK", 200
+
 
 if __name__ == "__main__":
     # Startup prometheus server
